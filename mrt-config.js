07480c8c8d084c872c2d42a644a342936d0fdc9f
Salvatore Musumeci <salvo.nos86@gmail.com>
2018-12-08 18:43:10 +0100

    Added function to config node to return to other nodes the status of the boiler

--- mrt-config.js
+++ mrt-config.js
@@ -8,11 +8,24 @@ module.exports = function(RED) {
     this.activities = config.activities
     var node = this
     this.current_profile = this.profiles[0] //#FIXME: deve pescare dallo storage su file
+    this.isBoilerOn = false
     this.setProfile = function(value){
       if (this.profiles.contains(value)){
         this.current_profile = value
         return true
       }else{ return false }
+    }
+    this.updateBoilerStatus = function(){
+      var date = new Date();
+      var minutes = (date.getHours() * 60 + date.getMinutes()) * 60 + date.getSeconds()
+      for (var i=0; i < this.activities.length; i++){
+        if (minutes > this.activities[i].from * 60 & minutes < this.activities[i].to * 60){
+          this.isBoilerOn = true
+          return true
+        }
+      };
+      this.isBoilerOn = false
+      return false
     }
   }
   RED.nodes.registerType("mrt-config",MRrT_Configuration);
