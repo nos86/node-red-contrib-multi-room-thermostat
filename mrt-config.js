22a702e69462e82647eea893dc93ebd228033fc8
Salvatore Musumeci <salvo.nos86@gmail.com>
2018-12-08 18:42:20 +0100

    Added variable to track the active profile in a central way

--- mrt-config.js
+++ mrt-config.js
@@ -6,6 +6,14 @@ module.exports = function(RED) {
     this.temperature = config.temperature
     this.profiles = config.profiles
     this.activities = config.activities
+    var node = this
+    this.current_profile = this.profiles[0] //#FIXME: deve pescare dallo storage su file
+    this.setProfile = function(value){
+      if (this.profiles.contains(value)){
+        this.current_profile = value
+        return true
+      }else{ return false }
+    }
   }
   RED.nodes.registerType("mrt-config",MRrT_Configuration);
 }
